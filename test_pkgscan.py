73c94470f5f8b1c77855243a85fb64c8fc86498d
John Speed Meyers <johnmeyersster@gmail.com>
2020-09-01 18:04:18 -0700

    Add test for get_last_release_date function

--- test_pkgscan.py
+++ test_pkgscan.py
@@ -8,12 +8,24 @@ def test_sort_semantic_version():
     assert sorted_list == ["0.1.1", "0.3.2", "5.5.5"]
 
 def test_PackageClass():
+    """Test instantiation of package class"""
     package = Package("requests")
 
 def test_get_first_release_date():
+    """Test get_first_release_date function"""
     package = Package("requests")
     assert package.get_first_release_date() == "2011-02-14"
     package = Package("numpy")
     assert package.get_first_release_date() == "2006-12-02"
+
+def test_get_last_release_date():
+    """Test get_last_release_date function
+    
+    To avoid breaking this test when there is a change to an often
+    updated package, this test uses pcap2map, a personal project of
+    John Speed Meyers on PyPI that is not maintained.
+    """
+    package = Package("pcap2map")
+    assert package.get_last_release_date() == "2020-04-10"
 
 
