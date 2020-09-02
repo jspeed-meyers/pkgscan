b49ae07400e7672a62b5c2c15849b90daca0cf70
John Speed Meyers <johnmeyersster@gmail.com>
2020-09-01 17:55:46 -0700

    Add helpers.py file

---
+++ helpers.py
@@ -0,0 +1,10 @@
+"""Helper functions
+
+
+"""
+from packaging import version
+
+def sort_semantic_version(unsorted_list):
+    """Sort a list of semantic version numbers"""
+    sorted_list = sorted(unsorted_list, key=lambda x: version.Version(x))
+    return sorted_list
