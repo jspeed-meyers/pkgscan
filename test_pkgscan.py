57ada442c6a90682d4388ee8c33bbf46516671e6
John Speed Meyers <johnmeyersster@gmail.com>
2020-09-01 17:56:34 -0700

    Add test for sorting semantic versions and change first_release dates

--- test_pkgscan.py
+++ test_pkgscan.py
@@ -1,12 +1,19 @@
 from main import Package
+from helpers import sort_semantic_version
+
+def test_sort_semantic_version():
+    """Test sort_semantic_version function"""
+    unsorted_list = ["0.3.2", "0.1.1", "5.5.5"]
+    sorted_list = sort_semantic_version(unsorted_list)
+    assert sorted_list == ["0.1.1", "0.3.2", "5.5.5"]
 
 def test_PackageClass():
     package = Package("requests")
 
 def test_get_first_release_date():
     package = Package("requests")
-    assert package.get_first_release_date() == "2012-01-22"
+    assert package.get_first_release_date() == "2011-02-14"
     package = Package("numpy")
-    assert package.get_first_release_date() == "2012-01-22"
+    assert package.get_first_release_date() == "2006-12-02"
 
 
