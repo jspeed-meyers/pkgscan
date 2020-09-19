d45ff36416e80494b9f8e08fff2bd14667bc943a
John Speed Meyers <johnmeyersster@gmail.com>
2020-09-19 10:52:58 -0700

    Refactor tests to use dicts created in Package initialization

--- test_pkgscan.py
+++ test_pkgscan.py
@@ -39,8 +39,8 @@ def test_package_class():
 
 def test_get_first_release_date():
     """Test get_first_release_date function"""
-    assert requests.first_release_date == "2011-02-14"
-    assert numpy.first_release_date == "2006-12-02"
+    assert requests.pypi_pkg["first_release_date"] == "2011-02-14"
+    assert numpy.pypi_pkg["first_release_date"] == "2006-12-02"
 
 
 def test_get_last_release_date():
@@ -50,40 +50,50 @@ def test_get_last_release_date():
     updated package, this test uses pcap2map, a personal project of
     John Speed Meyers on PyPI that is not maintained.
     """
-    assert pcap2map.last_release_date == "2020-04-10"
+    assert pcap2map.pypi_pkg["last_release_date"] == "2020-04-10"
 
 
 def test_get_number_versions():
     """Test get_number_versions function"""
-    assert pandas.number_versions >= 81
-    assert six.number_versions >= 27
-    assert pcap2map.number_versions == 1
+    assert pandas.pypi_pkg["number_versions"] >= 81
+    assert six.pypi_pkg["number_versions"] >= 27
+    assert pcap2map.pypi_pkg["number_versions"] == 1
 
 
 def test_get_author_email():
     """Test get_author_email function"""
-    assert portunus.author_email == "clewis@iqt.org"
+    assert portunus.pypi_pkg["author_email"] == "clewis@iqt.org"
 
 
 def test_get_author_name():
     """Test get_author_name function"""
-    assert networkml.author_name == "cglewis"
-    assert botocore.author_name == "Amazon Web Services"
-    assert matplotlib.author_name == "John D. Hunter, Michael Droettboom"
+    assert networkml.pypi_pkg["author_name"] == "cglewis"
+    assert botocore.pypi_pkg["author_name"] == "Amazon Web Services"
+    assert matplotlib.pypi_pkg["author_name"] == "John D. Hunter, Michael Droettboom"
 
 
 def test_get_home_page():
     """Test get_home_page function"""
-    assert urllib3.home_page == "https://urllib3.readthedocs.io/"
-    assert awscli.home_page == "http://aws.amazon.com/cli/"
-    assert pcap2map.home_page == "https://github.com/jspeed-meyers/pcap2map"
+    assert urllib3.pypi_pkg["home_page"] == "https://urllib3.readthedocs.io/"
+    assert awscli.pypi_pkg["home_page"] == "http://aws.amazon.com/cli/"
+    assert pcap2map.pypi_pkg["home_page"] == "https://github.com/jspeed-meyers/pcap2map"
+
+
+def test_is_pypi_package_signed():
+    """Test is_pypi_package_signed functionality"""
+    assert not pcap2map.pypi_pkg["pypi_pkg_signed"]
+    assert django.pypi_pkg["pypi_pkg_signed"]
 
 
 def test_get_pypi_maintainers_list():
     """Test get_pypi_maintainers_list function"""
-    assert pcap2map.maintainers_list == ["jspeed-meyers"]
-    assert networkml.maintainers_list == ["cglewis", "iqtlabsbot", "jspeed-meyers"]
-    assert pytest.maintainers_list == [
+    assert pcap2map.pypi_pkg["maintainers_list"] == ["jspeed-meyers"]
+    assert networkml.pypi_pkg["maintainers_list"] == [
+        "cglewis",
+        "iqtlabsbot",
+        "jspeed-meyers",
+    ]
+    assert pytest.pypi_pkg["maintainers_list"] == [
         "The_Compiler",
         "anatoly",
         "flub",
@@ -96,23 +106,25 @@ def test_get_pypi_maintainers_list():
 
 def test_get_pypi_maintainers_data():
     """Test get_pypi_maintainers_data function"""
-    assert len(portunus.maintainers_data) == 2
-    assert isinstance(portunus.maintainers_data[0], BeautifulSoup)
-    assert isinstance(portunus.maintainers_data[1], BeautifulSoup)
-    assert len(faucet.maintainers_data) == 1
-    assert isinstance(faucet.maintainers_data[0], BeautifulSoup)
-    assert len(ryu.maintainers_data) == 2
-    assert isinstance(ryu.maintainers_data[0], BeautifulSoup)
-    assert isinstance(ryu.maintainers_data[1], BeautifulSoup)
+    assert len(portunus.pypi_profiles["maintainers_data"]) == 2
+    assert isinstance(portunus.pypi_profiles["maintainers_data"][0], BeautifulSoup)
+    assert isinstance(portunus.pypi_profiles["maintainers_data"][1], BeautifulSoup)
+    assert len(faucet.pypi_profiles["maintainers_data"]) == 1
+    assert isinstance(faucet.pypi_profiles["maintainers_data"][0], BeautifulSoup)
+    assert len(ryu.pypi_profiles["maintainers_data"]) == 2
+    assert isinstance(ryu.pypi_profiles["maintainers_data"][0], BeautifulSoup)
+    assert isinstance(ryu.pypi_profiles["maintainers_data"][1], BeautifulSoup)
 
 
 def test_get_maintainers_account_creation_date():
     """Test get_maintainers_account_creation_date function"""
-    assert pcap2map.maintainers_account_creation_date == ["Nov 7, 2019"]
-    assert six.maintainers_account_creation_date == []
-    assert good.maintainers_account_creation_date == ["Jan 3, 2014"]
-    assert ryu.maintainers_account_creation_date == ["May 31, 2017"]
-    assert networkml.maintainers_account_creation_date == [
+    assert pcap2map.pypi_profiles["maintainers_account_creation_date"] == [
+        "Nov 7, 2019"
+    ]
+    assert six.pypi_profiles["maintainers_account_creation_date"] == []
+    assert good.pypi_profiles["maintainers_account_creation_date"] == ["Jan 3, 2014"]
+    assert ryu.pypi_profiles["maintainers_account_creation_date"] == ["May 31, 2017"]
+    assert networkml.pypi_profiles["maintainers_account_creation_date"] == [
         "Jul 24, 2020",
         "Nov 7, 2019",
     ]
@@ -120,9 +132,15 @@ def test_get_maintainers_account_creation_date():
 
 def test_get_number_of_packages_maintained_by_maintainers():
     """Test get_number_of_packages_maintained_by_maintainers"""
-    assert pcap2map.number_of_packages_maintained_by_maintainers == ["2"]
-    assert networkml.number_of_packages_maintained_by_maintainers == ["9", "3", "2"]
-    assert matplotlib.number_of_packages_maintained_by_maintainers == [
+    assert pcap2map.pypi_profiles["number_of_packages_maintained_by_maintainers"] == [
+        "2"
+    ]
+    assert networkml.pypi_profiles["number_of_packages_maintained_by_maintainers"] == [
+        "9",
+        "3",
+        "2",
+    ]
+    assert matplotlib.pypi_profiles["number_of_packages_maintained_by_maintainers"] == [
         "3",
         "38",
         "17",
@@ -133,33 +151,36 @@ def test_get_number_of_packages_maintained_by_maintainers():
 
 def test_get_github_page():
     """Test get_github_page function"""
-    assert pcap2map.github_page == "https://github.com/jspeed-meyers/pcap2map"
-    assert networkml.github_page == "https://github.com/IQTLabs/NetworkML"
-    assert requests.github_page == "https://github.com/psf/requests"
+    assert (
+        pcap2map.github_page_data["github_page"]
+        == "https://github.com/jspeed-meyers/pcap2map"
+    )
+    assert (
+        networkml.github_page_data["github_page"]
+        == "https://github.com/IQTLabs/NetworkML"
+    )
+    assert requests.github_page_data["github_page"] == "https://github.com/psf/requests"
 
 
 def test_get_github_data():
     """Test get_github_data function"""
     try:
-        assert pcap2map.github_data["id"] == 254624727
+        assert pcap2map.github_page_data["github_data"]["id"] == 254624727
     except KeyError:
         pass
     try:
-        assert scapy.github_data["id"] == 254624727
+        assert scapy.github_page_data["github_data"]["id"] == 254624727
     except KeyError:
         pass
 
 
 def test_get_github_stars():
     """Test get_github_stars function"""
-    assert pcap2map.github_stars == 0 or pcap2map.github_stars == "Rate limiting"
-    if isinstance(requests.github_stars, int):
-        assert requests.github_stars >= 43518
-    elif isinstance(requests.github_stars, str):
-        requests.github_stars == "Rate limiting"
-
-
-def test_is_pypi_package_signed():
-    """Test is_pypi_package_signed functionality"""
-    assert not pcap2map.pypi_pkg_signed
-    assert django.pypi_pkg_signed
+    assert (
+        pcap2map.github_page_data["github_stars"] == 0
+        or pcap2map.github_page_data["github_stars"] == "Rate limiting"
+    )
+    if isinstance(requests.github_page_data["github_stars"], int):
+        assert requests.github_page_data["github_stars"] >= 43518
+    elif isinstance(requests.github_page_data["github_stars"], str):
+        requests.github_page_data["github_stars"] == "Rate limiting"
